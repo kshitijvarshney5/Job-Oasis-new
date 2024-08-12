ff6210f495df8cfa5e6c6a4a974e6fd629fdc78d
kshitijvarshney5 <kshitijvarshney51@outlook.com>
2024-08-13 00:52:51 +0530

    config fix

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+const __dirname = path.resolve();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/hooks"),
+    },
+  },
   server: {
     port: 3000,
     proxy: {
