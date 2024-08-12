e7f9e919a2978af3dc6feb2afd86c3ae942d51d8
kshitijvarshney5 <kshitijvarshney51@outlook.com>
2024-08-13 01:17:27 +0530

    fix

--- Backend/index.js
+++ Backend/index.js
@@ -39,7 +39,7 @@ app.use("/api/v1/application", applicationRoute);
 
 app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "/Frontend/dist/index.html"));
 });
 
 app.listen(PORT, () => {
--- Frontend/src/components/admins/Companies.jsx
+++ Frontend/src/components/admins/Companies.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import Navbar from "../shared/NavBar.jsx";
 import { Input } from "../ui/input.jsx";
