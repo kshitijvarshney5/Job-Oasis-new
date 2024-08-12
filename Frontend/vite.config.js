f3b17f48b18f0c38ad4eb3ed23d0b931b86e64f1
kshitijvarshney5 <kshitijvarshney51@outlook.com>
2024-08-13 00:05:29 +0530

    minor fix

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,13 +1,15 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // eslint-disable-next-line no-undef
-      "@": path.resolve(__dirname, "./src"),
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+      },
     },
   },
 });
