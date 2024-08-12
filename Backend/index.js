36b1fbf4a1d152896a29edd8bdd0051cfc7c8a72
kshitijvarshney5 <kshitijvarshney51@outlook.com>
2024-08-12 19:03:26 +0530

    cors configuration updated

--- Backend/index.js
+++ Backend/index.js
@@ -17,11 +17,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true,
-};
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (process.env.allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept"
+  );
+  next();
+});
+
 const PORT = process.env.PORT || 3000;
 
 //api's
